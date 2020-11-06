437ffb43b5b292fc3f19d7e0ad591594e372f5d7
Roque Castro <rokke1558@gmail.com>
2020-11-06 09:46:46 -0500

    Paso de datos en actividades

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,27 @@
+import 'dart:async';
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/my_home_page.dart';
+import 'package:flutter_app/pages/second_page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(),
+      title: 'Material App',
+      //DEFINIR LAS  RUTTAS DE LA PAGINA
+      //home: MyHomePage());
+      initialRoute: "/",
+      routes: {
+        "/":(BuildContext context)=> MyHomePage(),
+        "/second":(BuildContext context)=> SecondPage(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage();
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "Material app",
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Uso del scaffold"),
-          ),
-          body: Center(
-              child: Image.network(
-            "https://static.iris.net.co/soho/upload/images/2020/2/10/61074_1.jpg",
-            height: 100,
-          )),  //endBODY
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {},
-          ),
-          drawer: Drawer(),
-        ));
-  }
-}
